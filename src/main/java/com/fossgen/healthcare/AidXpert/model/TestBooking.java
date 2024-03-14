3288614f981e9d00b7b3db8f09919d66ee008d8a
krvspk <krvspkumar1@gmail.com>
2024-03-14 14:29:03 +0530

    Validation changes

---
+++ src/main/java/com/fossgen/healthcare/AidXpert/model/TestBooking.java
@@ -0,0 +1,215 @@
+package com.fossgen.healthcare.AidXpert.model;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Table(name = "test_booking")
+@DynamicUpdate
+public class TestBooking implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer bookingId;
+
+	@Column(name = "booking_for")
+	private String bookingFor;
+
+	@Column(name = "patient_name")
+	private String patientName;
+
+	@Column(name = "doctor_name")
+	private String doctorName;
+
+	@Column(name = "booking_date")
+	private String bookingDate;
+
+	@Column(name = "prescription")
+	private String prescription;
+
+	@Column(name = "confirmed")
+	private String confirmed;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@Column(name = "payment_method", nullable = false)
+	private String paymentMethod;
+
+	@Column(name = "terms_conditions", nullable = false, length = 1)
+	private String termsAndConditions;
+
+	private String insurance;
+
+	private String reason;
+
+	private String symtoms;
+
+	private String version;
+
+	@Column(name = "ip_address")
+	private String ipAddress;
+
+	@Column(name = "created_by")
+	private String createdBy;
+
+	@Column(name = "created_date")
+	private Timestamp createdDate;
+
+	public TestBooking() {
+	}
+
+	public String getPatientName() {
+		return patientName;
+	}
+
+	public void setPatientName(String patientName) {
+		this.patientName = patientName;
+	}
+
+	public String getDoctorName() {
+		return doctorName;
+	}
+
+	public void setDoctorName(String doctorName) {
+		this.doctorName = doctorName;
+	}
+
+	public String getPrescription() {
+		return prescription;
+	}
+
+	public void setPrescription(String prescription) {
+		this.prescription = prescription;
+	}
+
+	public String getConfirmed() {
+		return confirmed;
+	}
+
+	public void setConfirmed(String confirmed) {
+		this.confirmed = confirmed;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getIpAddress() {
+		return ipAddress;
+	}
+
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Timestamp getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Timestamp createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+	public String getTermsAndConditions() {
+		return termsAndConditions;
+	}
+
+	public void setTermsAndConditions(String termsAndConditions) {
+		this.termsAndConditions = termsAndConditions;
+	}
+
+	public String getInsurance() {
+		return insurance;
+	}
+
+	public void setInsurance(String insurance) {
+		this.insurance = insurance;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	public String getSymtoms() {
+		return symtoms;
+	}
+
+	public void setSymtoms(String symtoms) {
+		this.symtoms = symtoms;
+	}
+
+	public Integer getBookingId() {
+		return bookingId;
+	}
+
+	public void setBookingId(Integer bookingId) {
+		this.bookingId = bookingId;
+	}
+
+	public String getBookingFor() {
+		return bookingFor;
+	}
+
+	public void setBookingFor(String bookingFor) {
+		this.bookingFor = bookingFor;
+	}
+
+	public String getBookingDate() {
+		return bookingDate;
+	}
+
+	public void setBookingDate(String bookingDate) {
+		this.bookingDate = bookingDate;
+	}
+
+}
